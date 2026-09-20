83eb599c84d8a46f6883e15b78ac30c6cb1563f0
Yuxu (6*7) <174954619+YX-2a@users.noreply.github.com>
2026-09-20 21:03:42 +0100

    lets goo a goal

--- interface.py
+++ interface.py
@@ -3,38 +3,46 @@ from tkinter import Canvas
 class Lever:
     def __init__ (self):
         self.actif_ = ""
-        self.switc_ = False
+        self.switc_ = None
+        self.num_ = 0
+        self.canvas = ""
         
-    def create_levers (self,canvas,height,width): ##dont play with switc_ or you will suffer
-        self.cords = [width//2 - width//10,height*(10/13),width//2 + width//10,height*(10/13) + height//8]
-        self.cords2 = [width//2 - width//10,height//10,width//2 + width//10,height//10 + height//8]
+    def create_levers (self,canvas,height,width,pow_): ##dont play with switc_ or you will suffer
+        self.canvas = canvas
+        self.pow_ = pow_
+        self.cords2 = [width//2 - width//10,height*(10/13),width//2 + width//10,height*(10/13) + height//8]
+        self.cords = [width//2 - width//10,height//10,width//2 + width//10,height//10 + height//8]
         self.color = "#000000"
         self.color2 = self.color
-        self.actif_ = canvas.create_rectangle (self.cords, fill = self.color) ## because of this bitch
-        self.switc_ = False
-        
-        def change_ (heh):
-            if self.switc_:
-                if heh.x  in range (round(self.cords2[0]), round(self.cords2[2])) and heh.y in range (round(self.cords2[1]),round(self.cords2[3])):
-                    canvas.delete (self.actif_)
-                    self.actif_ = canvas.create_rectangle (self.cords, fill = self.color)
+        self.actif_ = self.canvas.create_rectangle (self.cords2, fill = self.color) ## because of this bitch
+        self.switc_ = True
 
-                    self.switc_ = False
-                    
-                else:
-                    None
-                    
+    def change_ (self, heh):
+        if self.switc_:
+            if heh.x  in range (round(self.cords2[0]), round(self.cords2[2])) and heh.y in range (round(self.cords2[1]),round(self.cords2[3])):
+                self.canvas.delete (self.actif_)
+                self.actif_ = self.canvas.create_rectangle (self.cords, fill = self.color)
+
+                self.num_ = 2**self.pow_
+                self.switc_ = False
+
+                return self.num_
+            
             else:
-                if heh.x  in range (round(self.cords[0]), round(self.cords[2])) and heh.y in range (round(self.cords[1]),round(self.cords[3])):
-                    canvas.delete (self.actif_)
-                    self.actif_ = canvas.create_rectangle (self.cords2, fill = self.color2)
+                return None
+                
+        else:
+            if heh.x  in range (round(self.cords[0]), round(self.cords[2])) and heh.y in range (round(self.cords[1]),round(self.cords[3])):
+                self.canvas.delete (self.actif_)
+                self.actif_ = self.canvas.create_rectangle (self.cords2, fill = self.color2)
 
-                    self.switc_ = True
+                self.num_ = 2**self.pow_
+                self.switc_ = True
 
-                else:
-                    None
+                return -1*(self.num_)
 
-        canvas.bind ("<Button-1>", change_)
+            else:
+                return None
 
 class Interface:
     def __init__ (self):
--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__" :
     window = lr.Level()
     options = lr.read_level("./levels/level_1.ylfs")
     window.menu_()
-    window.make_level (options[0],options[1],options[2],options[3])
+    window.make_level (options[0],options[1],options[2],options[3],options[4])
     window.end_()
