ec9f108604e78d66bfcfe5fb35d4d4df20b2e745
Yuxu (6*7) <174954619+YX-2a@users.noreply.github.com>
2026-09-20 21:03:40 +0100

    much improvement in everything

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import level_reader as lr
 
 if __name__ == "__main__" :
     window = lr.Level()
-    options = lr.read_level("./levels/level_1.txt")
+    options = lr.read_level("./levels/level_1.ylfs")
     window.make_level (options[0],options[1],options[2],options[3])
     window.end_()
